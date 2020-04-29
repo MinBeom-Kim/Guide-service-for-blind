fddb27812e31f95fb978e5fc4572ce1fa1165d22
MinBeom_Kim <kmb3755@gmail.com>
2020-04-29 10:31:56 +0900

    kakao_api

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,18 @@
 const weather = document.querySelector(".js-weather");
-var container = document.getElementById('map');
 
 const API_KEY = "f5946ac701cd8a67bc89c1667083d24d"
-const API_KEY_KAKAO = "44d58646975a25fb4044f2b3ffacdd65"
 const COORDS = 'coords';
 
 function getMap(lat, lon) {
-    fetch(`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY_KAKAO}`
-    ).then(function (params) {
+    const div = document.createElement("div");
+    document.getElementById(map).onbeforeunload = function () {
         var options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(let, lon), //지도의 중심좌표.
+            center: new kakao.maps.LatLng(lat, lon), //지도의 중심좌표.
             level: 3 //지도의 레벨(확대, 축소 정도)
         };
 
-        var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    })
+        var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴 }
+    
 }
 
 function getWeather(lat, lon) {
